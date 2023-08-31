9d3856604fd2def27b44c5a5f85321fc71928f13
chensheng <1057791979@qq.com>
2023-08-31 17:57:08 +0800

    update pdf

---
+++ sort/MergeSort.java
@@ -0,0 +1,45 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 先令小数组有序，在合并有序的数组
+ */
+public class MergeSort {
+    private static Comparable[] aux;
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length-1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if(hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k < hi; k++) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k < hi; k++) {
+            if(i>mid) a[k] = aux[j++];
+            else if(j > hi) a[k] = aux[i++];
+            else if(SortUtil.less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] a = scanner.nextLine().split(" ");
+        sort(a);
+        assert SortUtil.isSorted(a);
+        SortUtil.show(a);
+    }
+}
---
+++ sort/QuickSort.java
@@ -0,0 +1,8 @@
+package sort;
+
+/**
+ * @author chenx
+ * @create 2023-07-24 10:15
+ */
+public class QuickSort {
+}
